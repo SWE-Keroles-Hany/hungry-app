be618b70e3fd0d8260ef45ba6b14e5d050985b79
KerolesHany <keroleshany293@gmail.com>
2026-01-15 11:09:31 +0200

    remove notes

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AppTheme {
   static const primaryColor = Color(0xff08431D);
   static const white = Colors.white;
   static const black = Colors.black;
   static const yellow = Colors.yellow;
-
   static const red = Colors.red;
   static const darkBrown = Color(0xff3C2F2F);
   static const Color mediumGray = Color(0xFF6A6A6A);
@@ -16,29 +14,24 @@ class AppTheme {
     appBarTheme: AppBarTheme(backgroundColor: white),
     bottomNavigationBarTheme: BottomNavigationBarThemeData(
       type: BottomNavigationBarType.fixed,
-
       backgroundColor: Colors.transparent,
     ),
-    sliderTheme: SliderThemeData(
-      trackHeight: 10.h,
 
-      // overlayShape: SliderComponentShape.noThumb,
-      valueIndicatorColor: black,
-    ),
+    sliderTheme: SliderThemeData(trackHeight: 10, valueIndicatorColor: black),
     textTheme: TextTheme(
       labelSmall: TextStyle(
         color: white,
-        fontSize: 16.sp,
+        fontSize: 16,
         fontWeight: FontWeight.bold,
       ),
       labelMedium: TextStyle(
         color: mediumGray,
-        fontSize: 20.sp,
+        fontSize: 20,
         fontWeight: FontWeight.bold,
       ),
       labelLarge: TextStyle(
         color: mediumGray,
-        fontSize: 25.sp,
+        fontSize: 25,
         fontWeight: FontWeight.bold,
       ),
     ),
